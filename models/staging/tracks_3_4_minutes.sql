7a848c900585b96eb520caad1afe9619373380c7
Ousmane Diallo <info.ouzdiallo@gmail.com>
2023-07-28 16:11:21 +0000

    finalizing part 1 of the project

--- models/staging/tracks_3_4_minutes.sql
+++ models/staging/tracks_3_4_minutes.sql
@@ -1,3 +1,19 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+with data_source as (
 select trackid, name, albumid, mediatypeid, genreid, milliseconds, bytes
 from {{ source("chinook_data", "tracks") }}
-where milliseconds >= 180000 and milliseconds <= 240000 and bytes > 6600000
+),
+
+final as (
+    select * from data_source
+    where milliseconds >= 180000 and milliseconds <= 240000 and bytes > 6600000
+)
+
+select * from final
+
+
--- models/staging/tracks_total.sql
+++ models/staging/tracks_total.sql
@@ -1,1 +1,5 @@
-select * from {{ source("chinook_data", "tracks") }}
+with data_source as (
+    select * from {{ source("chinook_data", "tracks") }}
+)
+
+select * from data_source
