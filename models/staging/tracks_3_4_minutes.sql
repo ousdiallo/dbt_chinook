8a14b731beaed688a528f009c83619d63a02f98d
Ousmane Diallo <info.ouzdiallo@gmail.com>
2023-07-29 00:29:03 +0000

    Window function and snapshot added

--- models/staging/tracks_3_4_minutes.sql
+++ models/staging/tracks_3_4_minutes.sql
@@ -1,19 +1,20 @@
 {{
-  config(
-    materialized = 'table',
+    config(
+        materialized="table",
     )
 }}
 
-with data_source as (
-select trackid, name, albumid, mediatypeid, genreid, milliseconds, bytes
-from {{ source("chinook_data", "tracks") }}
-),
+with
+    data_source as (
+        select trackid, name, albumid, mediatypeid, genreid, milliseconds, bytes
+        from {{ source("chinook_data", "tracks") }}
+    ),
 
-final as (
-    select * from data_source
-    where milliseconds >= 180000 and milliseconds <= 240000 and bytes > 6600000
-)
+    final as (
+        select *
+        from data_source
+        where milliseconds >= 180000 and milliseconds <= 240000 and bytes > 6600000
+    )
 
-select * from final
-
-
+select *
+from final
